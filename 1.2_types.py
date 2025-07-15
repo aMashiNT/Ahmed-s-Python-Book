184bdff24ecdb00cd19ce033a76948e2fba63d4d
Ahmed Mashroor <m7intiaf@gmail.com>
2025-07-16 00:05:26 +0600

    1.2 'type' examples

---
+++ 1.2_types.py
@@ -0,0 +1,11 @@
+# check_types.py
+
+x = "Ahmed"
+y = 29
+z = 5.4
+is_happy = True
+
+print(type(x))  # <class 'str'>
+print(type(y))  # <class 'int'>
+print(type(z))  # <class 'float'>
+print(type(is_happy))  # <class 'bool'>
