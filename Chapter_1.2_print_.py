a5cf0f375b4ba19b4fa0075feb223723c993cd17
Ahmed Mashroor <m7intiaf@gmail.com>
2025-07-15 22:43:41 +0600

    Add Chapter 1.2 print examples!

---
+++ Chapter_1.2_print_.py
@@ -0,0 +1,7 @@
+print("Hello, Pythonista Ahmed!")
+print("Welcome to Python learning journey.")
+
+
+print("Learning Python is fun!")
+print("I will become a great developer In Sha Allah.")
+print("Chapter 1 is about basic print statements.")
