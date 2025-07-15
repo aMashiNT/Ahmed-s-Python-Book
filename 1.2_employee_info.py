712853f1b5c4e92fe72579f86a1ec9567ee177dc
Ahmed Mashroor <m7intiaf@gmail.com>
2025-07-15 23:58:58 +0600

    1.2 employee info example

---
+++ 1.2_employee_info.py
@@ -0,0 +1,11 @@
+# employee_info.py
+
+name = "Ahmed"
+employee_id = 100
+salary = 150000.00
+is_active = True
+
+print(f"Name: {name}")
+print(f"ID: {employee_id}")
+print(f"Salary: {salary} taka")
+print(f"Currently working: {is_active}")
