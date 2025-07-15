6360eed0906cf7561fe04e624763a11ed63f0b80
Ahmed Mashroor <m7intiaf@gmail.com>
2025-07-16 00:11:55 +0600

    1.2 test

---
+++ 1.2_test.py
@@ -0,0 +1,17 @@
+#test.py
+
+# Declaring all types
+name = "Ahmed"
+age = 29
+height = 5.4
+is_learning = True
+
+# Checking and printing types
+print(f"Name: {name} (type: {type(name)})")
+print(f"Age: {age} (type: {type(age)})")
+print(f"Height: {height} (type: {type(height)})")
+print(f"Learning Python? {is_learning} (type: {type(is_learning)})")
+
+# Reassigning
+is_learning = False
+print("Learning status updated:", is_learning)
