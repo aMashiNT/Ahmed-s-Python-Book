20bb7388b3a646596dfbd296457251156efe7165
Ahmed Mashroor <m7intiaf@gmail.com>
2025-07-16 23:58:14 +0600

    Added comments

--- input.py
+++ input.py
@@ -2,3 +2,7 @@ name = input("What’s your name? ")
 city = input("Which city do you live in? ")
 
 print(f"Hi {name}, you live in {city}. That’s awesome!")
+
+# We asked the user for their name and city using input() and stored them in variables.
+# Then we printed a message using an f-string to include those variables in a sentence.
+# Output : Hi Ahmed, you live in Florida. That’s awesome!
