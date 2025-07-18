c1fdcce09c800d1da26a067320f6151f503b5343
Ahmed Mashroor <m7intiaf@gmail.com>
2025-07-18 18:16:17 +0600

    Animal guess game

---
+++ 1.2_animal_guess.py
@@ -0,0 +1,19 @@
+# animal guess game
+
+animal  = "Lion"
+animal1 = "Tiger"
+animal2 = "Fox"
+animal3 = "Buffalo"
+
+guess  =  input("Can you guess an animal? ")
+
+if guess == animal:
+    print("You nailed it!")
+else:
+    print("Try again!")    
+
+#The user has to use here capital letter "Lion" not "lion"
+#Otherwise "Try again!" will show everytime!
+# 
+# Correct input = Lion, Tiger, Fox, Buffalo
+# Wrong   input = lion, tiger, fox, buffalo 
