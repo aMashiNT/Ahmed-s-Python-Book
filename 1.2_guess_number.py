01456e7641bbee2ab738310d0f696aa3d7669bba
Ahmed Mashroor <m7intiaf@gmail.com>
2025-07-18 12:55:24 +0600

    guess number

---
+++ 1.2_guess_number.py
@@ -0,0 +1,10 @@
+# guess_number.py
+
+secret_number = 7
+
+guess = int(input("Guess a number between 1 and 10: "))
+
+if guess == secret_number:
+    print("You guessed it! MashaAllah!")
+else:
+    print("Wrong guess. Try again next time, in sha Allah.")
