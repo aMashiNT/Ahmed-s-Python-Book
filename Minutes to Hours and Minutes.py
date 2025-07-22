12d21208cf4225d95121bd5aa8179ea468c3a2ab
Ahmed Mashroor <m7intiaf@gmail.com>
2025-07-22 20:14:44 +0600

    1.4_Minutes to hours and minutes

---
+++ Minutes to Hours and Minutes.py
@@ -0,0 +1,8 @@
+# Minutes to Hours and Minutes
+
+total_minutes = int(input("Enter total minutes: "))
+
+hours = total_minutes // 60
+minutes = total_minutes % 60
+
+print("Time:", hours, "hour(s) and", minutes, "minute(s)")
