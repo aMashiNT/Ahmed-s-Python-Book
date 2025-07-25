0efc7bf2e5698ca5303b60e029a7bd43cca76fdd
Ahmed Mashroor <m7intiaf@gmail.com>
2025-07-25 23:50:40 +0600

    Check if a number is within a specific range

---
+++ 1.5_Check if a number is within a specific.py
@@ -0,0 +1,8 @@
+# Check if a number is within a specific range
+
+x = int(input("Enter a number: "))
+
+if 10 <= x <= 50:
+    print("Number is in the range 10 to 50")
+else:
+    print("Number is out of range")
