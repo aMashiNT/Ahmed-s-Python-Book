be4f71842e55c1e2c9f234f9b2071965dff3b6eb
Ahmed Mashroor <m7intiaf@gmail.com>
2025-07-29 22:48:47 +0600

    Find the largest of three numbers

---
+++ Practice_Find the largest of three numbers.py
@@ -0,0 +1,12 @@
+# Find the largest of three numbers
+
+a = 12
+b = 25
+c = 9
+
+if a >= b and a >= c:
+    print("Largest is", a)
+elif b >= a and b >= c:
+    print("Largest is", b)
+else:
+    print("Largest is", c)
