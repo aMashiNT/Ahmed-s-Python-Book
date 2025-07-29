a21b3e01dc8f6501c163cae94e8ecfdbb5133db3
Ahmed Mashroor <m7intiaf@gmail.com>
2025-07-29 22:28:08 +0600

    Practice even or odd

--- Practice_Check if a number is even or odd.py
+++ Practice_Check if a number is even or odd.py
@@ -1,8 +1,8 @@
 #  Check if a number is even or odd
 
-num = 21
+num = 36
 
-if num % 2 == 0:
+if num % 3 == 0:
     print(num, "is Even")
 else:
     print(num, "is Odd")
---
+++ Practice_PRACTICE EVEN OR ODD.py
@@ -0,0 +1,10 @@
+# PRACTICE EVEN OR ODD
+
+num = 22
+
+if num % 2 == 0:
+    print(num, "is Even")
+    for i in range(1, 12):
+        print(f"{"2"} x {i} = {2 * i}")
+else:
+    print(num, "is Odd")
