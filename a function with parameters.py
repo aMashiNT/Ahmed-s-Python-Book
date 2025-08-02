57455d391d5cf2e790920f551de7c36001141106
Ahmed Mashroor <m7intiaf@gmail.com>
2025-08-02 23:36:18 +0600

    A function with parameters

---
+++ a function with parameters.py
@@ -0,0 +1,8 @@
+
+def greet_person(name):
+    """This function greets the person passed in as an argument."""
+    print(f"Hello, {name}!")
+
+# Calling the function with an argument
+greet_person("Alice")
+greet_person("Bob")
