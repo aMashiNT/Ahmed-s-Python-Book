91073d63e1b5c9c81cba8951782179cd701a9d9f
Ahmed Mashroor <m7intiaf@gmail.com>
2025-08-04 23:51:06 +0600

    Check if number is even or odd

---
+++ Check if number is even or odd.py
@@ -0,0 +1,9 @@
+#Check if number is even or odd
+
+def check_even_odd(number):
+    if number % 2 == 0:
+        print(number, "is even")
+    else:
+        print(number, "is odd")
+
+check_even_odd(7)
