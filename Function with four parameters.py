c4f2dedae45e345fc028fc4bfed474562436bdfe
Ahmed Mashroor <m7intiaf@gmail.com>
2025-08-11 09:51:05 +0600

    Function with four parameters

---
+++ Function with four parameters.py
@@ -0,0 +1,11 @@
+
+
+
+def function_name(parameter1, parameter2, parameter3, parameter4):
+    # code block using parameters
+    print("Parameter 1:", parameter1)
+    print("Parameter 2:", parameter2)
+    print("Parameter 3:", parameter3)
+    print("Parameter 4:", parameter4)
+
+function_name("Ahmed", "Python", "Pythonista", "Coder")
