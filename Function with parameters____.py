be6e7ee2a863858c0d3e0e042b6e2c2acd7e25f5
Ahmed Mashroor <m7intiaf@gmail.com>
2025-08-11 09:41:22 +0600

    Function with parameters

---
+++ Function with parameters____.py
@@ -0,0 +1,9 @@
+#Function with parameters
+
+
+def function_name(parameter1, parameter2):
+    # code block using parameters
+    print("Parameter 1:", parameter1)
+    print("Parameter 2:", parameter2)
+
+function_name("Ahmed", "Python")
