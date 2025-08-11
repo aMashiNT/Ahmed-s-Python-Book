f85df75a43233be302e80bd876090c2608c216a5
Ahmed Mashroor <m7intiaf@gmail.com>
2025-08-11 09:57:25 +0600

    Function with return value

---
+++ Function with return value___.py
@@ -0,0 +1,10 @@
+#Function with return value
+#Instead of printing directly, send the result back to the caller
+
+
+
+def add(a, b):
+    return a + b
+
+result = add(5, 3)
+print("Sum is:", result)
